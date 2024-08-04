e9d6c0b4de01f43782c05d87d0d22fe5802946fd
JoaoLouceiro <jlouceiro@LIS-5CD1477XZH.dir.svc.accenture.com>
2024-08-04 13:04:41 +0100

    add tab formatting opt and catppuccin themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,25 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim", opts = {
-    transparent_mode = true,
-  } },
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      --transparent_mode = true,
+    },
+  },
+
+  {
+    "catppuccin",
+    opts = {
+      --transparent_background = true,
+    },
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox",
+      -- colorscheme = "catppuccin-frappe",
     },
   },
 }
